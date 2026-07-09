10a87da1c1991ceaaec730f15bcd0cd59d4c62d3
hey-its-aastha <valandastha@gmail.com>
2026-07-09 15:16:13 +0530

    linear algebra without model usage

--- algorithms/llinear_less_line.py
+++ algorithms/llinear_less_line.py
@@ -6,16 +6,8 @@ from sklearn import linear_model
 
 data = pd.read_csv('price.csv')
 
-#plt.scatter(data.area,data.price)
-#plt.show()
+plt.scatter(data.area,data.price)
+plt.show()
 
-#actual logoic
-#create and train model
-model = linear_model.LinearRegression()
-model.fit(data[['area']],data['price'])
-
-
-print(model.predict(3200))
-#y = b0
 
 
