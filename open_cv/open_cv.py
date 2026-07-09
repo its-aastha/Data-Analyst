5a053132b288deb9b21a4afd0f75a7aa01e76a2c
hey-its-aastha <valandastha@gmail.com>
2026-07-09 15:17:53 +0530

    video capture and store the image using opencv

---
+++ open_cv/open_cv.py
@@ -0,0 +1,25 @@
+import cv2
+video = cv2.VideoCapture(0)
+
+i = 1
+#r = photo click
+#q = quite
+
+while(True):
+    ret,frame = video.read() #if fps = 32 then it store the 32 image at tat time
+    cv2.imshow('frame',frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):                      #when you click q it quite the screen
+        break
+    elif key == ord('r'):                      #when you click the r then it store the image
+        filename = 'myimg_{}.jpg'.format(i)
+        cv2.imwrite(filename, frame)
+        print(f"{filename} saved successfully !")
+        i = i+1 
+        continue
+    
+video.release()
+cv2.destroyAllWindows()
+    
+
