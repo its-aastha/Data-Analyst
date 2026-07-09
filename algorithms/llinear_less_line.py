aa30d8d3e2df32b7672d5c1186d20244b4f81dea
hey-its-aastha <valandastha@gmail.com>
2026-07-09 11:29:40 +0530

    linear regresstion 3 lines code only show the graph not predict

---
+++ algorithms/llinear_less_line.py
@@ -0,0 +1,11 @@
+#day 7 
+#coding 3 line 
+import matplotlib.pyplot as plt  #data 
+import pandas as pd
+from sklearn import linear_model
+
+data = pd.read_csv('price.csv')
+
+plt.scatter(data.area,data.price)
+plt.show()
+
